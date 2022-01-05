889aa6bd20907fcfb820cdc1ceaee6b0b05d683b
Dan Russell <danruss@gmail.com>
2022-01-04 20:37:23 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # vermont-covid-count-notifier
+
+## Overview
+This serverless solution uses a `poller` lambda function to check the daily COVID-19 case data on the [Health Vermont API](https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/VIEW_EPI_DailyCount_PUBLIC_r3/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json) on a 5-minute interval.  If new data is found with the previous day's COVID-19 case count, it will add the item to DynamoDB.  
+
+A DynamoDB stream is configured for new items which invokes the `tweeter` function.  The `tweeter` function uses the Twitter API (via https://github.com/PLhery/node-twitter-api-v2) to send a tweet with the updated case counts.
+
+## Important Note
+The default configuration of the DynamoDB Stream will invoke the Lambda tweet function for every new item created.  If you don't want to spam tweet, disable the stream trigger temporarily until the initial dataset is loaded then re-enable the trigger.
+## Dependencies
+
+1. AWS account
+2. [Twitter API](https://developer.twitter.com/en/portal/products/elevated) Bearer Token with 'Elevated' access
+3. [AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html)
+
+## Deploying
+1. Set `CDK_DEFAULT_ACCOUNT` and `CDK_DEFAULT_REGION` environment variables with your AWS Account ID and preferred region.
+2. In the CDK folder: `npm run build` then `cdk deploy` (--profile {profile_name} to use a named AWS profile).
+2. In the AWS console or using the AWS CLI, modify the secret value in Secrets Manager to the value of your Twitter bearer token.
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,80 @@
+import * as path from 'path';
+
+import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import * as targets from '@aws-cdk/aws-events-targets';
+import * as sources from '@aws-cdk/aws-lambda-event-sources';
+import * as iam from '@aws-cdk/aws-iam';
+import { BillingMode, StreamViewType } from '@aws-cdk/aws-dynamodb';
+import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table to store our data
+    let dynamoTable = new dynamodb.Table(this, 'covid-tracker', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: {name: 'SK', type: dynamodb.AttributeType.NUMBER }, // Required for efficient sorting (get last item timestamp)
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      stream: StreamViewType.NEW_IMAGE
+    });
+
+    // Poller Lambda, permissions and trigger (every 5 min)
+    const pollerRole = new iam.Role(this, 'pollerRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    });
+    
+    pollerRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    dynamoTable.grantReadWriteData(pollerRole);
+
+    let poller = new lambda.Function(this, 'poller', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/poller')),
+      handler: 'poller.handler',
+      role: pollerRole,
+      environment: {
+        TABLE_NAME: dynamoTable.tableName,
+        STATE: 'Vermont'
+      },
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 1024
+    });
+
+    new Rule(this, 'pollerRule', {
+      schedule: Schedule.rate(cdk.Duration.minutes(5)),
+      targets: [new targets.LambdaFunction(poller)]
+    })
+
+    // Secret to store Twitter bearer token along with IAM role for Lambda Tweeter function
+    const secret = new secretsmanager.Secret(this, 'Secret');
+
+    const tweeterRole = new iam.Role(this, 'tweeterRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    });
+    
+    tweeterRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    secret.grantRead(tweeterRole);
+
+    // DynamoDB Stream function to tweet when new data received
+    let tweeter = new lambda.Function(this, 'tweeter', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/tweeter')),
+      handler: 'tweeter.handler',
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 1024,
+      role: tweeterRole,
+      environment: {
+        TWITTER_SECRET: secret.secretArn
+      }
+    });
+
+    tweeter.addEventSource(new sources.DynamoEventSource(dynamoTable, {
+      startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      batchSize: 1,
+      retryAttempts: 3
+    }));
+  }
+}
